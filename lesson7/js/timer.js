9fc84422f590308a479812fd2ff0218844038fc3
Svetlana <sionova2@gmail.com>
2018-08-22 14:49:38 +0300

    Добавлено дз к 7му уроку

---
+++ lesson7/js/timer.js
@@ -0,0 +1,20 @@
+let hours = document.getElementById('hours'),
+	minutes = document.getElementById('minutes'),
+	seconds = document.getElementById('seconds');
+
+
+
+function currentTime() {
+	let date = new Date(),
+	zeroMin = zeroSec = zeroHour = '';
+	if (date.getMinutes() < 10) zeroMin = '0';
+	if (date.getSeconds() < 10) zeroSec = '0';
+	if (date.getHours() < 10) zeroHour = '0';
+
+	hours.textContent = zeroHour + date.getHours() + ':',
+		minutes.textContent = zeroMin + date.getMinutes() + ':',
+		seconds.textContent = zeroSec + date.getSeconds();
+
+
+}
+setInterval(currentTime, 1);
